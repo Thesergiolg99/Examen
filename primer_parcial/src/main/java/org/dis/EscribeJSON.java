03ebaa49dc58976dec70fc05241f3917dc920c75
Sergio <sergiol199937@gmail.com>
2020-11-19 20:44:12 +0100

    Anieadido .txt con mi repo y user

--- primer_parcial/src/main/java/org/dis/EscribeJSON.java
+++ primer_parcial/src/main/java/org/dis/EscribeJSON.java
@@ -1,11 +1,11 @@
-//Fichero para mostrar por consola el JSON generado a partir del CSV y almacenarlo en el fichero best-golf-players.json
-package ...
-imports ...
+package org.dis;//Fichero para mostrar por consola el JSON generado a partir del CSV y almacenarlo en el fichero best-golf-players.json
+import com.opencsv.*;
+
+import java.io.FileReader;
+import java.util.Arrays;
+
 
 public class EscribeJSON {
 
-    public static void main(String[] args) {
-    ....
-    }
-    
- }
+
+}
